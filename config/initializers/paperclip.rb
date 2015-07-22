7a9f26019d7519bcd6804c9b98087c22b673aab4
trodrigu <trodriguez91@icloud.com>
2015-07-22 16:03:41 -0700

    change paperclip initializer to be correct class name

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,2 @@
-PaperClip::Attachment.default_options[:url] = ':s3_domain_url'
-PaperClip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
