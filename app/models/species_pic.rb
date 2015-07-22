0245b6c99f6b39c247276160551a3a7337bbc2d6
trodrigu <trodriguez91@icloud.com>
2015-07-22 16:34:02 -0700

    Add response content disposition attachment to species pic model

--- app/models/species_pic.rb
+++ app/models/species_pic.rb
@@ -1,7 +1,9 @@
 class SpeciesPic < ActiveRecord::Base
   belongs_to :species
   belongs_to :user
-  has_attached_file :avatar, :styles => { 
+  has_attached_file :avatar,
+    :s3_url_options => { :response_content_disposition => "attachment;" }, 
+    :styles => { 
     :medium => '300x300>', 
     :thumb => '100x100>' 
   }
